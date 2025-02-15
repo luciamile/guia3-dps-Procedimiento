43d9dcab7085f56971c4a033f074161379a6d85a
milelubonilla@gmail.com <milelubonilla@gmail.com>
2025-02-14 21:52:26 -0600

    Procedimiento
    Procedimiento de la guia 3

---
+++ Guia3/ejercicio1/src/app/components/Todo.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import styles from "../page.module.css"
+
+const Todo = ({todo, index, deleteTodo}) => {
+ return (
+    <>
+    <div className={styles.list}>
+    <h3>{todo}</h3>
+        <button className={styles.btn_delete} onClick={()=>deleteTodo(index)}>X</button>
+    </div>
+    </>
+ )
+}
+export default Todo
